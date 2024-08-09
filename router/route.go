28600f9d251b45459af53b384e80ce7d86f60b3e
Robby Sarvis <rjs.app.dev@proton.me>
2024-08-08 21:38:48 -0500

    feat/apple universal links

--- router/route.go
+++ router/route.go
@@ -4,6 +4,7 @@ import (
 	"basket-buddy-backend/ent"
 	"basket-buddy-backend/handler"
 	"basket-buddy-backend/middleware"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -13,6 +14,7 @@ func SetupRoutes(app *fiber.App, dbClient *ent.Client) {
 	api := app.Group("/api", logger.New())
 
 	setUpShareRoutes(api, dbClient)
+	setUpWellKnownRoutes(app)
 	api.Get("/health", handler.HealthEndpoint())
 
 }
@@ -23,4 +25,18 @@ func setUpShareRoutes(api fiber.Router, dbClient *ent.Client) {
 
 	config.Post("/", handler.CreateShareEndpoint(dbClient))
 	config.Get("/:ShareCode", handler.FetchShareEndpoint(dbClient))
+}
+
+func setUpWellKnownRoutes(app *fiber.App) {
+	wellKnown := app.Group("/.well-known")
+	wellKnown.Get("/apple-app-site-association", func(c *fiber.Ctx) error {
+		c.Set("Content-Type", "application/json; charset=utf-8")
+
+		content, err := os.ReadFile("apple-app-site-association.json")
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Error reading AASA file")
+		}
+
+		return c.Send(content)
+	})
 }
